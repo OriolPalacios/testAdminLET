b5483b704514daf86f6d90450e64166a48dca085
Oriol Palacios <orioloriolpalacios@outlook.com>
2024-10-08 11:46:46 -0500

    adminLTE template working

---
+++ resources/views/testing_admintlte.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Panel de control')
+
+@section('content_header')
+    <h1>Este es el Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido al panel de administrador.</p>
+@stop
